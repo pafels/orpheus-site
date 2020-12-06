9ea9756e06ef8659913e6fe85154460131aed77c
Vladimir Konushliev <v.konushliev@gmail.com>
2020-12-06 17:41:40 +0200

    Add the map of marked trails

--- src/pages/map.js
+++ src/pages/map.js
@@ -5,7 +5,10 @@ import SEO from "../components/seo"
 const MapPage = () => (
   <Layout>
     <SEO title="Карта" />
-    <h1>Карта</h1>
+    <h2>Карта на маркирани отсечки и маршрути в България</h2>
+    <div className="embed-responsive embed-responsive-1by1">
+      <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1N4eyP8qLkS3S-FI2UtY2lT6gJ-zPU_7a"></iframe>
+    </div>
   </Layout>
 )
 
